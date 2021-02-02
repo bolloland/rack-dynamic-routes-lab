1510fb621a9c1047e9ca005b5c8e6e483a1051cc
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-01 20:53:45 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,45 @@
+require 'pry'
+class Application
+
+    @@items = [Item.new("Figs",3.42), Item.new("Pears", 0.99)]
+
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            #binding.pry
+            @@items.map do |item|
+                i = req.path.split("/items/").last #=> "Figs"
+                # binding.pry
+                if item =@@items.find{|item_from_array| item_from_array.name == i}
+                    resp.write item.price
+                else 
+                    resp.status = 400
+                    resp.write "Item not found"
+                    
+                end
+            end
+                
+        else
+            resp.status = 404
+            resp.write "Route not found"
+        end
+        resp.finish
+    end
+end 
+
+          
+        #     item_name = req.params["q"]
+      
+        #     if @@items.include?(item_name)
+        #       resp.write "#{search_term} is one of our items"
+        #     else
+        #       resp.write "Couldn't find #{search_term}"
+        #     end
+      
+        #   else
+        #     resp.write "Path Not Found"
+        #   end
+      
